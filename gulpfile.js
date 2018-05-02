cab8b862858f628305145ad7d1dfa0840bf123a8
Moritz Ellmers <me@try-on.de>
2018-05-02 20:22:19 +0200

    added dev branch and deploy

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,16 @@ gulp.task('upload', function (done) {
         exclude: ['.git', '.idea', 'tmp/*']
     }, done);
 });
+
+gulp.task('uploadDev', function (done) {
+    ftpHelper.setup(plugins);
+    ftpHelper.upload({
+        username: "moritz",
+        password: "Ar76pm&4",
+        host: '46.228.205.175',
+        port: 21,
+        localRoot: 'build',
+        remoteRoot: '/dev',
+        exclude: ['.git', '.idea', 'tmp/*']
+    }, done);
+});
