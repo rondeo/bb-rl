9d1c0c30f8c96c6cdba0afce449ef284ee14c8b1
Moritz Ellmers <me@try-on.de>
2018-05-04 22:14:09 +0200

    - splitted login and loginWithThirdParty - added constants - added registration (without function)

--- src/index.js
+++ src/index.js
@@ -9,10 +9,78 @@ import 'bootstrap';
 import 'fullpage.js/dist/jquery.fullpage.min.css';
 import 'animate.css/animate.css';
 import iscroll from 'iscroll';
+
 window.jQuery = $;
 window.IScroll = iscroll;
 require('fullpage.js/vendors/scrolloverflow');
 require('fullpage.js');
+
+(function ($) {
+    $.fn.serializeObject = function () {
+
+        let self = this,
+            json = {},
+            push_counters = {},
+            patterns = {
+                "validate": /^[a-zA-Z][a-zA-Z0-9_]*(?:\[(?:\d*|[a-zA-Z0-9_]+)\])*$/,
+                "key": /[a-zA-Z0-9_]+|(?=\[\])/g,
+                "push": /^$/,
+                "fixed": /^\d+$/,
+                "named": /^[a-zA-Z0-9_]+$/
+            };
+
+
+        this.build = function (base, key, value) {
+            base[key] = value;
+            return base;
+        };
+
+        this.push_counter = function (key) {
+            if (push_counters[key] === undefined) {
+                push_counters[key] = 0;
+            }
+            return push_counters[key]++;
+        };
+
+        $.each($(this).serializeArray(), function () {
+
+            // skip invalid keys
+            if (!patterns.validate.test(this.name)) {
+                return;
+            }
+
+            let k,
+                keys = this.name.match(patterns.key),
+                merge = this.value,
+                reverse_key = this.name;
+
+            while ((k = keys.pop()) !== undefined) {
+
+                // adjust reverse_key
+                reverse_key = reverse_key.replace(new RegExp("\\[" + k + "\\]$"), '');
+
+                // push
+                if (k.match(patterns.push)) {
+                    merge = self.build([], self.push_counter(reverse_key), merge);
+                }
+
+                // fixed
+                else if (k.match(patterns.fixed)) {
+                    merge = self.build([], k, merge);
+                }
+
+                // named
+                else if (k.match(patterns.named)) {
+                    merge = self.build({}, k, merge);
+                }
+            }
+
+            json = $.extend(true, json, merge);
+        });
+
+        return json;
+    };
+})($);
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,9 +1,6 @@
 import React, {PureComponent} from 'react';
 import $ from 'jquery';
 
-import signInMs from './img/sign-in-with-microsoft-dark.png';
-import signInSteam from './img/sign-in-with-steam-dark.png';
-
 import './Login.css';
 
 export default class Login extends PureComponent {
@@ -11,49 +8,23 @@ export default class Login extends PureComponent {
     constructor(props) {
         super(props);
 
-        this.onSteamLogin = this.onSteamLogin.bind(this);
-
-        let steamUser = new RegExp('[?&]steamuser=([^&#]*)').exec(window.location.href);
-        if (steamUser !== null) {
-            steamUser = JSON.parse(decodeURIComponent(steamUser[1]));
-        }
-
-        this.state = {
-            steamUser: steamUser
-        };
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     componentDidMount() {
         window.scrollTo(0, 0);
     }
 
-    onSteamLogin() {
-        console.log("steam submit");
-
-        /*fetch("/public/steam/login.php", {
-            mode: "no-cors"
-        })
-            .then(function(response) {
-                console.log(response);
-                return response.json();
-            })
-            .then(function(json) {
-                console.log(json);
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
-            */
-    }
-
     onSubmit(e) {
         e.preventDefault();
-        console.log("submit", $("form").serializeArray());
+        let formData = $("form").serializeObject();
+
+        console.log("submit", formData);
 
         let headers = new Headers();
-        headers.append("Authorization", "Basic " + btoa("bbadmin:bbadminapi"));
+        headers.append("Authorization", "Basic " + btoa(formData.username + ":" + formData.password));
 
-        fetch("http://localhost:9000/user", {
+        fetch("http://localhost:9000/user/login", {
             headers: headers
         })
             .then(function (response) {
@@ -63,65 +34,36 @@ export default class Login extends PureComponent {
                 if (json.error) {
                     console.error("Status: " + json.status + ", Error: " + json.error + ", Message: " + json.message);
                 }
+                // Save data and greet user; save basic auth in cookie
                 console.log(json);
             })
             .catch(function (error) {
+                // Show error; user has to try again
                 console.log(error);
             });
     }
 
     render() {
-        let {steamUser} = this.state;
-        console.log(steamUser);
         return (
             <div className="container login">
+                <div className="row">
+                    <div className="col-12 col-md-6 offset-md-3">
+                        <h1>Login</h1>
 
-                <div className="login-wrapper">
-                    <h1>Turnieranmeldung</h1>
-
-                    <a href="http://localhost/battleground-bulls.de/public/steam/login.php?redirect=http://localhost:3000/anmelden">
-                        <img className="steam-login img-responsive"
-                             src={signInSteam}
-                             alt="Sign in with Steam"
-                             onClick={this.onSteamLogin}
-                        />
-                    </a>
-                    <a href="https://login.microsoftonline.com/common/oauth2/v2.0/authorize?
-                                client_id=16f74671-70ce-4e39-8d7f-e9f2e578f3b3
-                                &response_type=code
-                                &redirect_uri=http%3A%2F%2Flocalhost:3000
-                                &response_mode=query
-                                &scope=user.read
-                                &state=12345">
-                        <img className="microsoft-login img-responsive"
-                             src={signInMs}
-                             alt="Sign in with Microsoft"
-                        />
-                    </a>
-                       {/*
-                        <form className="form-signin" onSubmit={this.onSubmit.bind(this)}>
-
-                        <div className="form-label-group">
-                        <input type="email" id="inputEmail" name="mail" className="form-control" placeholder="E-Mail-Adresse" required="" autoFocus="" />
-                        <label htmlFor="inputEmail">E-Mail-Adresse</label>
-                        </div>
-
-                        <div className="form-label-group">
-                        <input type="password" id="inputPassword" name="password" className="form-control" placeholder="Passwort" required="" />
-                        <label htmlFor="inputPassword">Passwort</label>
-                        </div>
-
-                        <div className="checkbox mb-3">
-                        <label>
-                        <input type="checkbox" name="remember-me" value="remember-me" /> Remember me
-                        </label>
-                        </div>
-
-                        <button className="btn btn-lg btn-primary btn-block" type="submit">Anmelden</button>
+                        <form onSubmit={this.onSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="username">Benutzername</label>
+                                <input type="text" className="form-control" id="username" name="username" placeholder="Benutzername" required/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Passwort</label>
+                                <input type="password" className="form-control" id="password" name="password" placeholder="Passwort" required/>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Einloggen</button>
                         </form>
-                    */}
+                    </div>
                 </div>
             </div>
-    );
+        );
     }
-    }
+}
