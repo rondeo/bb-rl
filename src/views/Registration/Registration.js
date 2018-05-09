ef6ef8a00f1cabcda311237008c40b0fd41c1ff9
Moritz Ellmers <me@try-on.de>
2018-05-09 19:14:41 +0200

    added api with fetch method

--- src/views/Registration/Registration.js
+++ src/views/Registration/Registration.js
@@ -1,6 +1,8 @@
-import React, {PureComponent} from 'react';
-import {Link} from 'react-router-dom';
+import React, {PureComponent} from "react";
+import {Link} from "react-router-dom";
 import $ from "jquery";
+
+import API from "./../../utils/API";
 
 export default class Registration extends PureComponent {
 
@@ -42,24 +44,11 @@ export default class Registration extends PureComponent {
     onUsernameChange(e) {
         let input = $(e.target);
         clearTimeout(this.usernameChangeTimeout);
-        this.usernameChangeTimeout = setTimeout(function() {
+        this.usernameChangeTimeout = setTimeout(function () {
             let username = input.val();
-            console.log(username);
-            let headers = new Headers();
-            headers.append("Authorization", "Basic " + btoa("demo:demo"));
-            let formData = new FormData();
-            formData.append("username", username);
-            fetch("http://localhost:9000/user/checkUsername", {
-                headers: headers,
-                body: formData,
-                method: "POST"
-            })
+            API.getInstance()._fetch("/user/checkUsername", "POST", {username: username})
                 .then(function (response) {
-                    console.log(response.status);
-                    // if 409: show error, else: username ok
-                })
-                .catch(function (error) {
-                    console.log(error);
+                    console.log(response);
                 });
         }, 1000);
     }
@@ -74,24 +63,32 @@ export default class Registration extends PureComponent {
                         <form onSubmit={this.onSubmit}>
                             <div className="form-group">
                                 <label htmlFor="mail">E-Mail-Adresse</label>
-                                <input type="email" className="form-control" id="mail" aria-describedby="mailHelp" placeholder="E-Mail-Adresse"/>
-                                <small id="mailHelp" className="form-text text-muted">Wir werden deine E-Mail-Adresse niemals mit anderen teilen.</small>
+                                <input type="email" className="form-control" id="mail" aria-describedby="mailHelp"
+                                       placeholder="E-Mail-Adresse"/>
+                                <small id="mailHelp" className="form-text text-muted">Wir werden deine E-Mail-Adresse
+                                    niemals mit anderen teilen.
+                                </small>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="username">Benutzername</label>
-                                <input type="text" className="form-control" id="username" placeholder="Benutzername" onChange={this.onUsernameChange} required/>
+                                <input type="text" className="form-control" id="username" placeholder="Benutzername"
+                                       onChange={this.onUsernameChange} required/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="password1">Passwort</label>
-                                <input type="password" className="form-control" id="password1" placeholder="Passwort" required/>
+                                <input type="password" className="form-control" id="password1" placeholder="Passwort"
+                                       required/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="password2">Passwort wiederholen</label>
-                                <input type="password" className="form-control" id="password2" placeholder="Passwort wiederholen" required/>
+                                <input type="password" className="form-control" id="password2"
+                                       placeholder="Passwort wiederholen" required/>
                             </div>
                             <div className="form-check">
                                 <input type="checkbox" className="form-check-input" id="exampleCheck1" required/>
-                                <label className="form-check-label" htmlFor="exampleCheck1">Bitte akzeptiere unsere <Link to="/datenschutz" target="_blank">Datenschutzbestimmungen</Link>.</label>
+                                <label className="form-check-label" htmlFor="exampleCheck1">Bitte akzeptiere
+                                    unsere <Link to="/datenschutz"
+                                                 target="_blank">Datenschutzbestimmungen</Link>.</label>
                             </div>
                             <button type="submit" className="btn btn-primary">Registrierung abschließen</button>
                         </form>
