dc7be7a3e5b349f80bcff9986cb9a07057e5df5f
Moritz Ellmers <me@try-on.de>
2018-05-09 19:14:41 +0200

    added api with fetch method

---
+++ src/utils/API.js
@@ -0,0 +1,77 @@
+import * as CONSTANTS from "../constants";
+import $ from "jquery";
+
+export default class API {
+
+    /**
+     * @returns API instance
+     */
+    static getInstance() {
+        if (!this["singleton"]) {
+            this["singleton"] = new API();
+        }
+        return this["singleton"];
+    }
+
+    // Add Basic Auth to Cookie
+    login(data) {
+    }
+
+    // Remove Basic Auth from Cookie
+    logout() {
+    }
+
+    _fetch(url, method = "GET", body = null, query = null, header = {}, contentType = "application/json") {
+        let dfr = $.Deferred();
+
+        header = $.extend({}, {
+            "Accept": "application/json",
+            "Authorization": "Basic " + btoa(CONSTANTS.WEBCLIENT_NAME + ":" + CONSTANTS.WEBCLIENT_SECRET),
+            "Content-Type": contentType
+        }, header);
+        let headers = new Headers();
+        for (let key in header) {
+            if (!header.hasOwnProperty(key)) continue;
+            headers.append(key, header[key]);
+        }
+
+        if (url.indexOf("http") === -1) {
+            url = CONSTANTS.API_ROUTE + url;
+        }
+
+        if (query) {
+            url += "?";
+            let keys = Object.keys(query);
+            keys.forEach((key, index) => {
+                if (!query.hasOwnProperty(key)) return;
+                url += key + "=" + query[key];
+                if (keys.length-1 > index) {
+                    url += "&";
+                }
+            });
+        }
+
+        fetch(url, {
+            headers: headers,
+            body: body ? JSON.stringify(body) : null,
+            method: method
+        })
+            .then(this._parseJson)
+            .then(json => {
+                if (json.error) {
+                    console.error("Status: " + json.status + ", Error: " + json.error + ", Message: " + json.message);
+                    dfr.reject(json);
+                }
+                dfr.resolve(json);
+            })
+            .catch(error => {
+                console.error("Unexpected Error in API", error);
+                dfr.reject(error);
+            });
+        return dfr;
+    }
+
+    _parseJson(response) {
+        return response.status + "".indexOf("204", "309") > -1 ? { status: response.status } : response.json()
+    }
+}
