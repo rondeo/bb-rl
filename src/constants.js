ed3a983a38f589a1bd37ea908270ca0973bc446f
Moritz Ellmers <me@try-on.de>
2018-05-11 19:08:22 +0200

    registration extended and added styles

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
 export const API_ROUTE = "http://localhost:9000";
 export const WEBCLIENT_NAME = "webclient";
 export const WEBCLIENT_SECRET = "KqsL4vuFPKmA";
+
+export const ERROR_PASSWORD_NOT_EQUAL = "password_not_equal";
