9d8399137e0f3a5e1d23ccef18585c39a1ceca25
Moritz Ellmers <me@try-on.de>
2018-07-06 01:20:56 +0200

    Calendar switched to fullcalendar.js // styling

--- src/views/Calendar/Calendar.js
+++ src/views/Calendar/Calendar.js
@@ -1,20 +1,85 @@
 import React from "react";
+import "moment/min/locales.min";
+import "fullcalendar";
+import $ from "jquery";
 
+import "fullcalendar/dist/fullcalendar.min.css";
 import "./Calendar.css";
 
 export default class Calendar extends React.PureComponent {
 
-        render() {
-            return (
-                <div className="view full-container calendar">
-                    <div className="container">
-                        <h1>kalender</h1>
-                        <p>Patrick -> m4kar0ny     | 10.01.1985</p>
-                        <p>Patrick -> Zoomer       | 29.01.1997</p>
-                        <p>Daniel  -> Fennek112    | 04.02.1991</p>
+    componentDidMount() {
+        $(this.refs.calendar).fullCalendar({
+            bootstrapFontAwesome: {
+                //prev: 'fa-chevron-left',
+            },
+            businessHours: {
+                start: "10:00",
+                end: "22:00"
+            },
+            eventLimit: 3,
+            events: [
+                {
+                    title  : 'Geburtstag Jana/MrsMadness',
+                    start  : '2018-07-06'
+                },
+                {
+                    title  : 'Wochenende',
+                    start  : '2018-07-13',
+                    end    : '2018-07-16'
+                },
+                {
+                    title  : 'Termin mit Uhrzeit',
+                    start  : '2018-07-10T12:30:00'
+                },
+                {
+                    title  : 'Viele Termine an einem Tag',
+                    start  : '2018-07-25'
+                },
+                {
+                    title  : 'Viele Termine an einem Tag',
+                    start  : '2018-07-25'
+                },
+                {
+                    title  : 'Viele Termine an einem Tag',
+                    start  : '2018-07-25'
+                },
+                {
+                    title  : 'Viele Termine an einem Tag',
+                    start  : '2018-07-25'
+                },
+                {
+                    title  : 'Viele Termine an einem Tag',
+                    start  : '2018-07-25'
+                }
+            ],
+            footer: {
+                left: "prevYear,prev,next,nextYear today",
+                center: "",
+                right: "agendaDay,agendaWeek,month,list"
+            },
+            header: {
+                left: "prevYear,prev,next,nextYear today",
+                center: "title",
+                right: "agendaDay,agendaWeek,month,list"
+            },
+            locale: "de",
+            navLinks: true,
+            nowIndicator: true,
+            selectable: true,
+            themeSystem: "bootstrap4",
+            weekNumbers: true,
+            weekNumberTitle: ""
+        })
+    }
 
-                    </div>
+    render() {
+        return (
+            <div className="view full-container calendar">
+                <div className="container">
+                    <div ref="calendar" />
                 </div>
-            )
-        }
+            </div>
+        )
+    }
 }
