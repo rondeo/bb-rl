30331511080b2382c590a6e043a14654ff867c49
Moritz Ellmers <me@try-on.de>
2018-07-29 00:49:56 +0200

    merged

--- src/views/MyProfile/MyProfile.js
+++ src/views/MyProfile/MyProfile.js
@@ -1,8 +1,8 @@
 import React from "react";
-import $ from "jquery";
-import {Helmet} from "react-helmet";
-
 import DateTime from "react-datetime";
+import Helmet from "react-helmet";
+import $ from "jquery";
+
 import "react-datetime/css/react-datetime.css";
 
 import UsernameInput from "../../components/UsernameInput/UsernameInput";
