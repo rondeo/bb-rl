3531f40fe1ba590b6f50d261e125e285560b5ad9
Moritz Ellmers <me@try-on.de>
2018-07-29 01:04:40 +0200

    added missing link import

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,6 +1,7 @@
 import React, {PureComponent} from "react";
 import classnames from "classnames";
 import Helmet from "react-helmet";
+import {Link} from "react-router-dom";
 import $ from "jquery";
 
 import About from "../../components/About/About";
